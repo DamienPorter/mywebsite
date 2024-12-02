f7d64c003ff3171e98b7949bf00045045174314e
DamienPorter <damien_scott@hotmail.co.uk>
2024-12-02 10:48:48 +0000

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,33 @@ hamburger.addEventListener("click", () => {
 });
 
 // New Navbar js end
+
+// Smooth Scroll start
+
+// Function to determine the responsive offset
+function getResponsiveOffset() {
+  if (window.innerWidth < 768) {
+    return 32;
+  } else if (window.innerWidth < 1200) {
+    return 5;
+  } else {
+    return 2;
+  }
+}
+
+// Initialize SmoothScroll with a dynamic offset
+let scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 800,
+  offset: getResponsiveOffset(), // Set initial offset
+});
+
+// Update SmoothScroll settings on window resize
+window.addEventListener("resize", function () {
+  scroll.destroy(); // Destroy the current instance
+  scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 800,
+    offset: getResponsiveOffset(), // Update the offset dynamically
+  });
+});
+
+// Smooth Scroll end
